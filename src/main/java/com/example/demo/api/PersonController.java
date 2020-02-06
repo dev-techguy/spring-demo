6ddd587f49eb06a47b15a8f6b89df726e3e91777
dev-techguy <dev.techguy@gmail.com>
2020-02-06 22:58:55 +0300

    :hankey: Writing bad code that needs to be improved.

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,9 +3,13 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping(value = "person")
 public class PersonController {
     private final PersonService personService;
 
@@ -14,7 +18,7 @@ public class PersonController {
         this.personService = personService;
     }
 
-    public void addPerson(Person person) {
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
 }
