c34ed35c19d67f39def0cb6d48b6fc53cbea47a9
dev-techguy <dev.techguy@gmail.com>
2020-02-07 13:18:34 +0300

    :hankey: Writing bad code that needs to be improved.

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -16,5 +17,40 @@ public class FakePersonDataAccessService implements PersonDao {
     public int insertPerson(UUID uuid, Person person) {
         DB.add(new Person(uuid, person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID uuid) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(uuid))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID uuid) {
+        Optional<Person> person = selectPersonById(uuid);
+        if (person.isEmpty()) {
+            return 0;
+        }
+        DB.remove(person.get());
+        return 1;
+    }
+
+    @Override
+    public int updatePersonById(UUID uuid, Person person) {
+        return selectPersonById(uuid)
+                .map(p -> {
+                    int indexOfPersonToDelete = DB.indexOf(p);
+                    if (indexOfPersonToDelete >= 0) {
+                        DB.set(indexOfPersonToDelete, new Person(uuid, person.getName()));
+                        return 1;
+                    }
+                    return 0;
+                }).orElse(0);
     }
 }
--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -5,6 +5,10 @@ import com.example.demo.model.Person;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class PersonService {
     private final PersonDao personDao;
@@ -15,5 +19,21 @@ public class PersonService {
 
     public int addPerson(Person person) {
         return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID uuid) {
+        return personDao.selectPersonById(uuid);
+    }
+
+    public int deletePerson(UUID uuid) {
+        return personDao.deletePersonById(uuid);
+    }
+
+    public int updatePerson(UUID uuid, Person person) {
+        return personDao.updatePersonById(uuid, person);
     }
 }
